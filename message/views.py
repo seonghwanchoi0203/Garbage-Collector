ef380089698f6fcdde57b7fc3718e5d9c75610d5
Yipeng Zhang <zyp5511@gmail.com>
2018-06-03 17:27:35 -0700

    minor

--- message/views.py
+++ message/views.py
@@ -182,7 +182,7 @@ def buyer_accept(request):
                 ## another If here to do actual payment
                 garbage.sold = True
                 garbage.buyer = e_user
-                garbage.save()
+                garbage.save(force_update=True)
                 offer_out.title = "Success"
                 offer_out.content = "The Item has been successfully reserved"
                 offer_out.negotiate_price = negotiate_price
